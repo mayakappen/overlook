7b848c41936c58dd1edd4be0cb6124a3e72fcbc8
Maya Kappen <mayakappen@gmail.com>
2022-08-04 20:41:10 -0600

    add tests for booking properties

---
+++ test/Booking-test.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Booking', function () {
+    it('should be a class', function () {
+        expect(Booking).to.be.a(Class)
+    });
+
+    it.skip('should be an instantiate an instance of Booking', function() {
+        expect(newBooking).to.be.an.instanceOf(Booking)
+    });
+
+    it.skip('should hold an id', function() {
+        expect(newBooking.id).to.equal("5fwrgu4i7k55hl6sz")
+    })
+    it.skip("should have a userID", function () {
+        expect(newBooking.userID).to.equal(9)
+    })
+    it.skip("should have a date", function () {
+        expect(newBooking.date).to.equal("2022/04/22")
+    })
+    it.skip("should have a roomNumber", function() {
+        expect(newUser.roomNumber).to.equal(15)
+    })
+ 
+    it.skip('')
+});
