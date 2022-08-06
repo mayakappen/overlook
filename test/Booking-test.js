c504aa2ee121758c64eb41fff1ac749a48421d38
Maya Kappen <mayakappen@gmail.com>
2022-08-05 19:02:11 -0600

    create Booking class

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -36,23 +36,23 @@ describe('Booking', () => {
         expect(Booking).to.be.a('function')
     });
 
-    it.skip('should be an instantiate an instance of Booking', () => {
+    it('should be an instantiate an instance of Booking', () => {
         expect(newBooking).to.be.an.instanceOf(Booking)
     });
 
-    it.skip('should hold an id', () => {
+    it('should hold an id', () => {
         expect(newBooking.id).to.equal("5fwrgu4i7k55hl6sz")
     });
-    it.skip("should have a userID", () => {
+    it("should have a userID", () => {
         expect(newBooking.userID).to.equal(9)
     });
-    it.skip("should have a date", () => {
+    it("should have a date", () => {
         expect(newBooking.date).to.equal("2022/04/22")
     });
-    it.skip("should have a roomNumber", () => {
+    it("should have a roomNumber", () => {
         expect(newBooking.roomNumber).to.equal(15)
     });
-    it.skip("should have a cost", () => {
+    it("should have a cost", () => {
         newBooking.getCost(room)
         expect(newBooking.cost).to.equal(294.56)
     });
