0ab69bca15373e698ab2e4c17c086753a4569b7e
Maya Kappen <mayakappen@gmail.com>
2022-08-05 18:47:21 -0600

    actually put things into the right script file

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,31 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import Booking from '../src/classes/Booking.js'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let fetchData = (data) => {
+    return fetch(`http://localhost:3001/api/v1/${data}`)
+        .then(rsp => rsp.json())
+        .catch(error => console.log(error))
+};
+
+let customers;
+let bookings;
+let rooms;
+let customer;
+let booking;
+let room;
+
+function getPromises() {
+    Promise.all([fetchData('customers'), fetchData('bookings'), fetchData('rooms')]).then(data => {
+        customers = data[0].customers;
+        console.log('customers: ', customers);
+        bookings = data[1].bookings;
+        console.log('bookings: ', bookings);
+        rooms = data[2].rooms;
+        console.log('rooms: ', rooms);
+        booking = new Booking(bookings[0]);
+    })
+}
+
+window.addEventListener('load', getPromises);
